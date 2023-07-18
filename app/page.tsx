fc88b01524b419da81ec453f9165bfbd6b1b193d
ntafra00 <ntafra00@fesb.hr>
2023-07-19 00:15:57 +0200

    landing page - WIP

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,60 @@
-import Image from 'next/image'
+"use client"
+
+import Navbar from "@/components/commons/navbar";
+import Footer from "@/components/commons/footer";
+import GradientLayout from "@/layouts/GradientLayout";
+
+import { Flex, Heading, Text, Image, Button, useDisclosure, IconButton, Box } from "@chakra-ui/react";
+
+import { RightArrow, DownArrow } from "@/components/commons/icons";
+
 
 export default function Home() {
+  const { isOpen, onOpen, onClose } = useDisclosure()
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    <>
+      <Navbar />
+      <GradientLayout>
+        <Flex direction={{ base: 'column', md: 'row' }} p={20}>
+          <Flex direction='column' gap={4} w={{ base: '100%', md: '50%' }} justifyContent={{ base: 'end', md: 'center' }}>
+            <Heading color='white'>Enjoy and learn at the same time, what kind of a spell is that?</Heading>
+            <Text color='white'>Create a quiz without a wand or saying the magic words</Text>
+            <Button colorScheme='purple' rounded='full' color='white' width='sm'>Join the Quizzers now!</Button>
+          </Flex>
+          <Flex w={{ base: '100%', md: '50%' }} justifyContent='center' alignItems='center' pt={{ base: 20, md: 0 }}>
+            <Image src='mainSection-1.png' alt="Hero image one" />
+          </Flex>
+        </Flex >
+        <Flex direction={{ base: 'column', md: 'row' }} pt={0}>
+          <Flex w={{ base: '100%', md: '50%' }} h='80%' justifyContent='center' alignItems='center' pt={{ base: 20, md: 0 }}>
+            <Image src='mainSection-2.png' alt="Hero image one" />
+          </Flex>
+          <Flex direction='column' gap={4} w={{ base: '100%', md: '50%' }} justifyContent={{ base: 'end', md: 'center' }}>
+            <Flex>
+              <IconButton
+                size={'lg'}
+                icon={isOpen ? <RightArrow /> : <DownArrow />}
+                aria-label={'Open Menu'}
+                onClick={isOpen ? onClose : onOpen}
+                bgColor='transparent'
+                color='white'
+                _hover={{}}
+              />
+              <Heading color='white'>Create</Heading>
+            </Flex>
+            <Flex width='50%' visibility={isOpen ? 'visible' : 'hidden'} direction='column' gap={2}>
+              <Flex width='50%' direction='column' gap={2}>
+                <Text color='white'>Choose the quiz type and get started immediately</Text>
+                <Button bg={'white'} rounded='full' color='purple' w='75%'>Learn more</Button>
+              </Flex>
+              <Box borderTop={'1px solid white'}><Heading color='white'>Play</Heading></Box>
+              <Box borderTop={'1px solid white'}><Heading color='white'>Compete</Heading></Box>
+            </Flex>
+          </Flex >
+        </Flex >
+      </GradientLayout>
+      <Footer />
+    </>
   )
 }
