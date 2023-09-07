d29ce770b9a5299cc4a19c65075c0fa4922bfc0a
ivantomi <ivan.luka.1234@gmail.com>
2023-09-07 09:24:55 +0200

    Typo

--- app/quizzes/components/StoriesAndQuizCTA.tsx
+++ app/quizzes/components/StoriesAndQuizCTA.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import { Box, Center, Heading, Flex } from "@/components/chakra";
 import { QuizInfo } from "./QuizInfo";
@@ -6,30 +6,54 @@ import { COMMON_PAGE_PADDING } from "@/constants";
 import { useRouter } from "next/navigation";
 
 export function StoriesAndQuizCTA() {
+  const router = useRouter();
 
-    const router = useRouter();
+  const handleRedirectToStories = () => {
+    router.push("/stories");
+  };
 
-    const handleRedirectToStories = () => {
-        router.push("/stories");
-    }
+  const handleScrollToQuizzes = () => {
+    document.getElementById("quizzes")?.scrollIntoView();
+  };
 
-    const handleScrollToQuizzes = () => {
-        document.getElementById("quizzes")?.scrollIntoView();
-    }
-
-    const handleRedirectToQuizCreation = () => {
-        router.push("/quizzes/createQuiz")
-    }
-    return (
-        <Box pt="16px" px={COMMON_PAGE_PADDING} pb="68px">
-            <Center pb="56px">
-                <Heading color="black" fontSize={{ base: "md", lg: "l" }} fontWeight="semibold" textAlign="center">Choose visely - each path offers something important of value!</Heading>
-            </Center>
-            <Flex direction={{ base: "column", lg: "row" }} justifyContent="space-between">
-                <QuizInfo text="Check out other quizzes" handleOnClick={handleScrollToQuizzes} imageDescription="Spells book" imageUrl="/spellsBook.png" />
-                <QuizInfo text="Get inspired by our stories" handleOnClick={handleRedirectToStories} imageDescription="Broken glasses" imageUrl="/brokenGlasses.png" />
-                <QuizInfo text="Make your own quiz" handleOnClick={handleRedirectToQuizCreation} imageDescription="Potion" imageUrl="/potion.png" />
-            </Flex>
-        </Box>
-    )
+  const handleRedirectToQuizCreation = () => {
+    router.push("/quizzes/createQuiz");
+  };
+  return (
+    <Box pt="16px" px={COMMON_PAGE_PADDING} pb="68px">
+      <Center pb="56px">
+        <Heading
+          color="black"
+          fontSize={{ base: "md", lg: "l" }}
+          fontWeight="semibold"
+          textAlign="center"
+        >
+          Choose wisely - each path offers something important of value!
+        </Heading>
+      </Center>
+      <Flex
+        direction={{ base: "column", lg: "row" }}
+        justifyContent="space-between"
+      >
+        <QuizInfo
+          text="Check out other quizzes"
+          handleOnClick={handleScrollToQuizzes}
+          imageDescription="Spells book"
+          imageUrl="/spellsBook.png"
+        />
+        <QuizInfo
+          text="Get inspired by our stories"
+          handleOnClick={handleRedirectToStories}
+          imageDescription="Broken glasses"
+          imageUrl="/brokenGlasses.png"
+        />
+        <QuizInfo
+          text="Make your own quiz"
+          handleOnClick={handleRedirectToQuizCreation}
+          imageDescription="Potion"
+          imageUrl="/potion.png"
+        />
+      </Flex>
+    </Box>
+  );
 }
